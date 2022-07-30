9ecf274f3c519460ee8db5c5345c1ecd8786d532
Christiaan Bloemendaal <github@tnrd.net>
2022-07-30 20:51:44 +0200

    feat: added project

---
+++ Zeepkist-LevelEditorMovementSpeed/Patches/LEV_MoveCamera_TranslateCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace TNRD.Zeepkist.LevelEditorMovementSpeed.Patches
+{
+    [HarmonyPatch(typeof(LEV_MoveCamera), "TranslateCamera")]
+    public class LEV_MoveCamera_TranslateCamera
+    {
+        private static FieldInfo useMoveSpeedField;
+
+        private static void Prefix(LEV_MoveCamera __instance)
+        {
+            if (useMoveSpeedField == null)
+            {
+                useMoveSpeedField =
+                    typeof(LEV_MoveCamera).GetField("useMoveSpeed", BindingFlags.Instance | BindingFlags.NonPublic);
+            }
+
+            float useMoveSpeed = (float)useMoveSpeedField.GetValue(__instance);
+            useMoveSpeedField.SetValue(__instance, useMoveSpeed * Plugin.Multiplier);
+        }
+    }
+}
---
+++ Zeepkist-LevelEditorMovementSpeed/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace TNRD.Zeepkist.LevelEditorMovementSpeed
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static int Multiplier { get; private set; } = 1;
+
+        private ConfigEntry<KeyCode> plusKey;
+        private ConfigEntry<KeyCode> minusKey;
+
+        private Harmony harmony;
+
+        private void Awake()
+        {
+            harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            plusKey = Config.Bind(new ConfigDefinition("General", "plusKey"),
+                KeyCode.KeypadPlus,
+                ConfigDescription.Empty);
+            minusKey = Config.Bind(new ConfigDefinition("General", "minusKey"),
+                KeyCode.KeypadMinus,
+                ConfigDescription.Empty);
+
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+            harmony = null;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(plusKey.Value))
+            {
+                Multiplier++;
+            }
+
+            if (Input.GetKeyDown(minusKey.Value))
+            {
+                Multiplier--;
+
+                if (Multiplier < 1)
+                    Multiplier = 1;
+            }
+        }
+    }
+}
